d7c2936bc62b8269f32146213b46203df553c59b
SehrishHussain <sehrishaslam1@hotmail.com>
2025-06-24 18:07:13 +0500

    NextAuth setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import '@/styles/global.css'
 import { ReactNode } from 'react'
+import AuthProvider from './context/AuthProvider'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <AuthProvider>
       <body>{children}</body>
+      </AuthProvider>
     </html>
   )
 }
